3c139e491fe9eff9a4267e4d8a800e6c69d1459d
Fernando Reta <fernandoreta@MacBooks-MacBook-Pro.local>
2019-08-23 21:48:33 -0500

    applying routes in my components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,49 +7,9 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  @ViewChild('f') signupForm: NgForm;
-  defaultQuestion = 'teacher';
-  answer = '';
-  genders = ['male', 'female'];
-  user = {
-    username: '',
-    email: '',
-    secretQuestion: '',
-    answer: '',
-    gender: ''
-  };
-  submitted = false;
+ 
 
-  suggestUserName() {
-    const suggestedName = 'Superuser';
-    // this.signupForm.setValue({
-    //   userData: {
-    //     username: suggestedName,
-    //     email: ''
-    //   },
-    //   secret: 'pet',
-    //   questionsAnswer: '',
-    //   gender: 'male'
-    // });
-    this.signupForm.form.patchValue({
-      userData: {
-        username: suggestedName
-      }
-    });
-  }
+  showForms() {
 
-  // onSubmit(form: NgForm){
-  //   console.log(form);
-  // }
-
-  onSubmit(){
-    this.submitted = true;
-    this.user.username = this.signupForm.value.userData.username;
-    this.user.email = this.signupForm.value.userData.email;
-    this.user.secretQuestion = this.signupForm.value.userData.secret;
-    this.user.answer = this.signupForm.value.userData.questionsAnswer;
-    this.user.gender = this.signupForm.value.userData.gender;
-
-    this.signupForm.reset();
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { FormsComponent } from './forms/forms.component';
+import { ServicesComponentComponent } from './services-component/services-component.component';
+
+const appRoutes: Routes = [
+  { path: 'app-forms',
+    component: FormsComponent
+  },
+  { path: 'app-services-component',
+    component: ServicesComponentComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormsComponent,
+    ServicesComponentComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule,
     FormsModule,
     HttpModule
