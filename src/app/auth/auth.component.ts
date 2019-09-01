11b25dfd273aa42447183270b56525b192f8e7e3
Fernando <fer19_reta@hotmail.com>
2019-08-31 23:07:52 -0500

    login and catch errors

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { AuthService } from './auth.service';
+import { AuthService, AuthResponseData } from './auth.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html'
 })
 export class AuthComponent implements OnInit {
-
+  
   constructor(private authService: AuthService) { }
   isLoading = false;
   isLoginMode = false;
@@ -20,26 +21,30 @@ export class AuthComponent implements OnInit {
     // This is if the user disable the required from the dev tools
     if (!myForm.valid) { return; }
 
+    let authObs: Observable<AuthResponseData>;
     const email = myForm.value.email;
     const password = myForm.value.password;
     this.isLoading = true;
+
     if (this.isLoginMode) {
-      // ...
-    } else {
-      // because return something in authservice IN THE POST LINE this is an observable
-      this.authService.signUp(email, password).subscribe(resData => {
-        console.log(resData);
-        this.isLoading = false;
-      }, error => {
-        this.errorMsg = 'Something is wrong!';
-        this.isLoading = false;
-      });
-      myForm.reset();
-    }
+      authObs = this.authService.login(email, password);
+    } else   {
+      authObs = this.authService.signUp(email, password);
     }
 
+    authObs.subscribe(
+      resData => {
+        console.log(resData);
+        this.isLoading = false;
+    },
+      errorMes => {
+        console.log(errorMes);
+        this.errorMsg = errorMes;
+        this.isLoading = false;
+      });
+        myForm.reset();
+      }
 
   ngOnInit() {
   }
-
-}
+      }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { catchError, onErrorResumeNext } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
-interface AuthResponseData {
+export interface AuthResponseData {
     idToken: string;
     email: string;
     refreshToken: string;
     expiresIn: string;
     localId: string;
+    registered?: boolean;
 }
 
 @Injectable({
@@ -24,6 +27,27 @@ export class AuthService {
             password: password,
             returnSecureToken: true
         }
-        );
+        ).pipe(catchError(errorRes => {
+            /** Catching the error on a service with PIPE*/
+            const errorMessage = 'An error ocurred!';
+            if (!errorRes.error || !errorRes.error.error) {
+                return throwError(errorMessage);
+            }
+            switch (errorRes.error.error.message) {
+                case 'EMAIL_EXISTS':
+                  return throwError('Email already exists!');
+              }
+        }));
+    }
+
+    /**Diffretn path for login same API KEY */
+    login(email: string, password: string) {
+        return this.http.post<AuthResponseData>(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBnVlIEnl1JnuZsv0lmTVlHHcXY9p29OnA',
+            {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        });
     }
 }
