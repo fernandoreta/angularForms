7508ad77edfba47cd0fc289c80ed165b925b9ecb
Fernando <fer19_reta@hotmail.com>
2019-09-07 11:58:42 -0500

    info post and fetch data

--- src/app/info/info.component.ts
+++ src/app/info/info.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-info',
@@ -8,13 +9,23 @@ import { NgForm } from '@angular/forms';
 })
 export class InfoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  submitForm(myInfo: NgForm) {
-    console.log(myInfo.value);
+  submitForm(myInfo: {title: string, content: string}) {
+    this.http.post('https://ng-http-2b26c.firebaseio.com/myinfo.json', myInfo)
+      .subscribe(resData => {
+        console.log(resData);
+      });
   }
 
+  private getFetch() {
+    this.http.get('https://ng-http-2b26c.firebaseio.com/myinfo.json')
+      .subscribe(resData => {
+        console.log(resData);
+      });
+  }
   ngOnInit() {
+    this.getFetch();
   }
 
 }
